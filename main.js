864945e08e140eb54447913554c2bf31b696e645
Tahir <dibirovtahir@gmail.com>
2022-12-10 22:21:09 +0400

    #

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const select = document.querySelector('.filter-region')
 const cards = document.querySelector('.cards')
 const miniCards = document.querySelector('.card')
 const dataURL = 'https://restcountries.com/v3.1/all'
-const selector  = document.querySelector('select')
+const selector = document.querySelector('select')
 
-selector.addEventListener('change', ()=>{
-   
-    searchCountry(searchBox .value, dataURL, cardGenerator, cards)
+selector.addEventListener('change', () => {
+
+    searchCountry(searchBox.value, dataURL, cardGenerator, cards)
     !searchBox.value && (fillCards(dataURL, cardGenerator, cards))
-    
+
 })
 
 
@@ -23,7 +23,7 @@ searchBox.addEventListener('input', (event) => {
 })
 
 const cardGenerator = (imageURL, title, population, region, capital) => {
-    
+
     return (
         `
         <div class="card">
@@ -39,15 +39,6 @@ const cardGenerator = (imageURL, title, population, region, capital) => {
     )
 }
 
-// const getCard = async(dataURL, render, mount, target)=>{
-//     const response =  await fetch(dataURL)
-//     const data = await response.json()
-//     targetCard = data.filter(item => {
-//         return item.name.common = target
-//     })
-// }
-
-
 
 
 
@@ -59,33 +50,26 @@ const fillCards = async (dataURL, render, mount) => {
     data.forEach(item => {
 
         mount.innerHTML += render(item.flags.svg, item.name.common, item.population, item.continents, item.capital[0])
-        
-        // document.querySelectorAll('.card').forEach(card=>{
-        //     card.addEventListener('click', (event)=>{
-        //      
-        //      console.log(keyValue)
-        //       document.getElementById('root').innerHTML+=new Card().render(item.flags.svg, item.name.common, item.population, item.continents, item.capital[0])
-        //     })
-        // })
 
-
-        document.querySelectorAll('.card').forEach(card=>{
-            card.addEventListener('click', (event)=>{
+        document.querySelectorAll('.card').forEach(card => {
+            card.addEventListener('click', (event) => {
                 let keyValue = event.target.querySelector('.card-title').textContent
                 console.log(keyValue)
                 let thatCountry = data.filter(item => item.name.common === keyValue)[0]
-            
+
                 console.log(thatCountry)
                 console.log(document.getElementById('details'))
-                document.getElementById('details').innerHTML+=new Card().render(thatCountry.flags.svg, thatCountry.name.common, thatCountry.population, thatCountry.continents, thatCountry.capital[0])
+                sessionStorage.setItem('card', JSON.stringify(thatCountry))
+                window.location.href = '/details.html'
+                document.getElementById('details').innerHTML += new Card().render(thatCountry.flags.svg, thatCountry.name.common, thatCountry.population, thatCountry.continents, thatCountry.capital[0])
 
             })
         })
-        
+
 
     })
 
-   
+
 
 
 }
@@ -95,25 +79,14 @@ const searchCountry = async (targetCountry, dataURL, render, mount) => {
     const response = await fetch(dataURL)
     const data = await response.json()
     const country = data.filter(item => {
-        // if(selector.value==='all') selector.value = true
-        console.log(selector.value)
-       return item.name.common.toUpperCase() === targetCountry.toUpperCase() && selector.value === "all" ? true : item.continents[0].toUpperCase() === selector.value
-})
-    console.log(country)
-    mount.innerHTML = render(country[0].flags.svg, country[0].name.common, country[0].population, country[0].continents, country[0].capital[0])
+
+        return item.name.common.toUpperCase() === targetCountry.toUpperCase() && selector.value === "all" ? true : item.continents[0].toUpperCase() === selector.value
+    })
+    country.forEach(country => {
+        mount.innerHTML = render(country.flags.svg, country.name.common, country.population, country.continents, country.capital[0])
+    })
+
 }
-
-
-// const searchByRegions = async (region, dataURL, render, mount) => {
-//     const response = await fetch(`${dataURL}/${region}`)
-//     const data = await response.json()
-//     data.forEach(item => {
-
-//         mount.innerHTML += render(item.flags.svg, item.name.common, item.population, item.continents, item.capital[0])
-
-//     })
-// }
-
 
 
 
